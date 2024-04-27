2e3705d74542d77005111f16183d3f36d119c2b5
Priyanshu Singh <priyanshusingh349@gmail.com>
2024-04-27 18:38:04 +0530

    added precommit hook

--- models.py
+++ models.py
@@ -1,13 +1,15 @@
-from sqlalchemy import Table, Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
+
 
 class Book(Base):
     __tablename__ = "books"
     id = Column(Integer, primary_key=True)
     title = Column(String(length=255))
     author_id = Column(Integer, ForeignKey("authors.id"))
+
 
 class Author(Base):
     __tablename__ = "authors"
